823e7347aa9f9a77e4d6fffb83841286b001d447
DarioLopes <dario.lopes@iatec.com>
2025-08-25 11:35:35 -0300

    back-end finalizado

--- myapp/src/middlewares/tasksMiddleware.js
+++ myapp/src/middlewares/tasksMiddleware.js
@@ -1,4 +1,4 @@
-const validateBody = (req, res, next) => {
+const validateFieldTitle = (req, res, next) => {
     const { body } = req;
     if (body.title === undefined) {
         return res.status(400).json({ message: '"title" is required' });
@@ -8,7 +8,18 @@ const validateBody = (req, res, next) => {
         next();
     }
 };
+const validateFieldStatus = (req, res, next) => {
+    const { body } = req;
+    if (body.status === undefined) {
+        return res.status(400).json({ message: '"status" is required' });
+    } else if (body.status === '') {
+        return res.status(400).json({ message: '"status" cannot be empty' });
+    } else {
+        next();
+    }
+};
 
 module.exports = {
-    validateBody,
+    validateFieldTitle,
+    validateFieldStatus
 };
