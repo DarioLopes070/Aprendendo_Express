af7411016e923c224d43268f28ae151c3452578c
DarioLopes <dario.lopes@iatec.com>
2025-08-26 17:32:06 -0300

    implementações feitas

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,5 +1,6 @@
 const tbody = document.querySelector('tbody');
 const addForm = document.querySelector('.add-form');
+const inputTask = document.querySelector('.input-task');
 
 const fetchTasks = async () => {
     const response = await fetch('http://localhost:3000/tasks')
@@ -7,8 +8,16 @@ const fetchTasks = async () => {
     return tasks
 }
 
-const addTask = async (task) => {
-    alert(teste);
+const addTask = async (event) => {
+    event.preventDefault();
+    const task = { title: inputTask.value }
+    await fetch('http://localhost:3000/tasks', {
+        method: 'post',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(task),
+    });
+    loadTasks();
+    inputTask.value = '';
 }
 
 const createElement = (tag, innerText = '', innerHTML = '') => {
@@ -24,7 +33,7 @@ const createElement = (tag, innerText = '', innerHTML = '') => {
 
 const createSelect = (value) => {
     const options = `
-    <option value="pendente">pendente</option>
+    <option value="pending">pendente</option>
     <option value="em andamento">em andamento</option>
     <option value="Concluido">concluído</option>
     `;
@@ -34,6 +43,12 @@ const createSelect = (value) => {
 
 }
 
+const formatDate = (dateUTC) => {
+    const options = {DataStyle: 'long', TimeStyle: 'short'};
+    const date = new Date(dateUTC).toLocaleString(options);
+    return date;
+
+}
 const task = {
     id: 1,
     title: 'Estudar Node.js',
@@ -45,7 +60,7 @@ const createRow = (task) => {
 
     const tr = createElement('tr');
     const tdTitle = createElement('td', title);
-    const tdCreatedAt = createElement('td', created_at);
+    const tdCreatedAt = createElement('td', formatDate(created_at));
     const tdStatus = createElement('td');
     const tdActions = createElement('td');
     const select = createSelect(status);
@@ -71,6 +86,7 @@ const createRow = (task) => {
 
 const loadTasks = async () => {
     const tasks = await fetchTasks();
+    tbody.innerHTML = '';
     tasks.forEach((task) => createRow(task));
 }
 
